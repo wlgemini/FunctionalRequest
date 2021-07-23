f14e0b1f74da082e597e7e9402c4251da962bf3d
wlgemini <wangluguang@live.com>
2021-07-23 18:50:36 +0800

    .

--- Sources/API/API+DataRequest.swift
+++ Sources/API/API+DataRequest.swift
@@ -143,8 +143,10 @@ public extension API {
 }
 
 
+// MARK: Context for DataRequest
 extension API {
     
+    /// 生成method
     func _method(for ctx: Context) -> Alamofire.HTTPMethod? {
         guard let method = ctx.dataRequest.api.method else {
             _Log.error("HTTPMethod not set, request won't start", location: ctx.requestLocation)
@@ -154,6 +156,7 @@ extension API {
         return method
     }
     
+    /// 生成url
     func _url(for ctx: Context) -> String? {
         guard let initialURL = ctx.dataRequest.api.initialURL else {
             _Log.error("Initial URL not set, request won't start", location: ctx.requestLocation)
@@ -186,4 +189,119 @@ extension API {
         
         return url
     }
+    
+    /// 合并Headers，self.additionalHeaders会覆盖Configuration.headers中冲突的key-value
+    func _headers(for ctx: Context) -> Alamofire.HTTPHeaders {
+        // combinedHeaders init from `Config.DataRequest.headers()` or an empty headers
+        var combinedHeaders = Store._api.dataRequest.headers ?? Alamofire.HTTPHeaders()
+        
+        // additionalHeaders override or appends to combinedHeaders
+        for h in ctx.dataRequest.headers {
+            combinedHeaders.add(h)
+        }
+        
+        return combinedHeaders
+    }
+    
+    /// encoder
+    func _encoder(for ctx: Context) -> Alamofire.ParameterEncoder {
+        if let encoder = ctx.dataRequest.encoder {
+            return encoder
+            
+        } else {
+            guard let method = ctx.dataRequest.api.method else {
+                return Alamofire.URLEncodedFormParameterEncoder.default
+            }
+            
+            switch method {
+            // URLEncodedFormParameterEncoder
+            case .get: return Store._api.dataRequest.encoder.get ?? Alamofire.URLEncodedFormParameterEncoder.default
+            case .delete: return Store._api.dataRequest.encoder.delete ?? Alamofire.URLEncodedFormParameterEncoder.default
+                
+            // JSONEncoding
+            case .patch: return Store._api.dataRequest.encoder.patch ?? Alamofire.JSONParameterEncoder.default
+            case .post: return Store._api.dataRequest.encoder.post ?? Alamofire.JSONParameterEncoder.default
+            case .put: return Store._api.dataRequest.encoder.put ?? Alamofire.JSONParameterEncoder.default
+                
+            // default
+            default: return Alamofire.URLEncodedFormParameterEncoder.default
+            }
+        }
+    }
+    
+    /// encoding
+    func _encoding(for ctx: Context) -> Alamofire.ParameterEncoding {
+        if let encoding = ctx.dataRequest.encoding {
+            return encoding
+            
+        } else {
+            guard let method = ctx.dataRequest.api.method else {
+                return Alamofire.URLEncoding.default
+            }
+            
+            switch method {
+            // URLEncoding
+            case .get: return Store._api.dataRequest.encoding.get ?? Alamofire.URLEncoding.default
+            case .delete: return Store._api.dataRequest.encoding.delete ?? Alamofire.URLEncoding.default
+                
+            // JSONEncoding
+            case .patch: return Store._api.dataRequest.encoding.patch ?? Alamofire.JSONEncoding.default
+            case .post: return Store._api.dataRequest.encoding.post ?? Alamofire.JSONEncoding.default
+            case .put: return Store._api.dataRequest.encoding.put ?? Alamofire.JSONEncoding.default
+                
+            // default
+            default: return Alamofire.URLEncoding.default
+            }
+        }
+    }
+    
+    /// 修改URLRequest
+    func _urlRequestModifiers(for ctx: Context) -> (_ req: inout URLRequest) throws -> Void {
+        // NOTE: Make sure not access `Context` in block
+        return { [urlRequestModifiers = ctx.dataRequest.urlRequestModifiers] (req) in
+            for modifier in urlRequestModifiers {
+                try modifier(&req)
+            }
+        }
+    }
+    
+    /// Authentication
+    func _authenticate(for ctx: Context) -> URLCredential? {
+        ctx.dataRequest.authenticate
+    }
+    
+    /// Redirect
+    func _redirectHandler(for ctx: Context) -> Alamofire.RedirectHandler? {
+        return ctx.dataRequest.redirectHandler ?? Store._api.dataRequest.redirect
+    }
+}
+
+
+// MARK: Context for DataResponse
+extension API {
+    
+    /// Validate DataResponse
+    func _acceptableStatusCodes(for ctx: Context) -> Range<Int>? {
+        ctx.dataResponse.acceptableStatusCodes ?? Store._api.dataResponse.acceptableStatusCodes
+    }
+    
+    func _acceptableContentTypes(for ctx: Context) -> [String]? {
+        let types = ctx.dataResponse.acceptableContentTypes ?? Store._api.dataResponse.acceptableContentTypes
+        return types?()
+    }
+
+    // Serialize DataResponse
+//    var serializeData = Store.SerializeData()
+//    var serializeString = Store.SerializeString()
+//    var serializeJSON = Store.SerializeJSON()
+//    var serializeDecodable = Store.SerializeDecodable()
+//    
+//    // Cache DataResponse
+//    var cacheHandler: Alamofire.CachedResponseHandler?
+}
+
+// MARK: Context for Accessing
+extension API {
+//    var onDataRequestAvailable: Available<Alamofire.DataRequest>?
+    
 }
