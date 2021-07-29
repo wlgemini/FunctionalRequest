7f579808a135e9028a8e6ebaa2db018735b39518
wlgemini <wangluguang@live.com>
2021-07-29 19:32:08 +0800

    .

---
+++ Tests/Models.swift
@@ -0,0 +1,22 @@
+////
+////  Models.swift
+////  FunctionalRequestTests
+////
+////  Created by wangluguang on 2021/3/2.
+////
+
+import Foundation
+
+
+struct Echo: Codable, Equatable {
+    let foo: String
+    let bar: String
+    let baz: String
+}
+
+enum Models {
+
+    static let echo = Echo(foo: "x", bar: "y", baz: "z")
+    
+    static let echoJSON = ["foo": "x", "bar": "y", "baz": "z"]
+}
